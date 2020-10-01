6ad14d65e98ef4f48a9de5f693dea446ef2c2285
Ajay Nayr <ajaynayr@gmail.com>
2020-10-01 12:52:17 -0500

    Added a giphy link

--- server/routes/giphy.router.js
+++ server/routes/giphy.router.js
@@ -2,6 +2,23 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
+// router.get('/:term', (req, res) => {
+    
+//     axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${req.params.term}`)
+//         .then(response => {
+//             console.log(response.data);
+//             res.send(response.data)
+//         })
+//         .catch(err => {
+//             console.log(err);
+//             res.sendStatus(500)
+
+//         })
+//         console.log(req.body);
+
+
+// });
+
 
 router.get('/', (req, res) => {
     axios({
